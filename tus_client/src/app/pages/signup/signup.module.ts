8c03a43bdb5cb3833a82a412ef016ffaa2f09ea0
Adesegun Koiki <kamar3deen@gmail.com>
2023-04-02 18:06:09 +0100

    signup UI completed

--- tus_client/src/app/pages/signup/signup.module.ts
+++ tus_client/src/app/pages/signup/signup.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SignupComponent } from './signup.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { RouterModule } from '@angular/router';
 
 
 
@@ -9,7 +10,8 @@ import { SharedModule } from 'src/app/shared/shared.module';
   declarations: [SignupComponent],
   imports: [
     CommonModule,
-    SharedModule
+    SharedModule,
+    RouterModule.forChild([{'path': '', component:SignupComponent}])
   ],
   exports: [
     SignupComponent
